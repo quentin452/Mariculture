7eb49636e83e3a249b7dd3d334242480813cfcc1
quentin452 <42176772+quentin452@users.noreply.github.com>
2024-01-02 17:21:02 +0100

    Update WorldGenReef.java

--- src/main/java/mariculture/world/decorate/WorldGenReef.java
+++ src/main/java/mariculture/world/decorate/WorldGenReef.java
@@ -3,6 +3,7 @@ package mariculture.world.decorate;
 import hacker.TimeMeasurement;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 import mariculture.core.Core;
@@ -18,14 +19,6 @@ import net.minecraft.world.chunk.Chunk;
 import net.minecraft.world.gen.feature.WorldGenerator;
 
 public class WorldGenReef extends WorldGenerator {
-    private void setBlock(ArrayList<String> cache, World world, int x, int y, int z) {
-        String coords = x + ":" + z;
-        if (!cache.contains(coords)) {
-            cache.add(coords);
-        }
-
-        world.setBlock(x, y, z, Core.rocks);
-    }
 
     public boolean coralCanReplace(Block block) {
         return block == Blocks.sand || block == Blocks.gravel || block == Blocks.dirt || block == Core.limestone;
@@ -42,24 +35,42 @@ public class WorldGenReef extends WorldGenerator {
             TimeMeasurement.start("CoralReef");
         }
 
-        ArrayList<String> cache = new ArrayList<>();
+        ArrayList<Integer> coralPositions = new ArrayList<>();
 
         y = world.getTopSolidOrLiquidBlock(x, z);
 
         if (WorldGen.CORAL_EXTRA) {
-            for (int i = -rand.nextInt(16) - rand.nextInt(4); i < rand.nextInt(16) + rand.nextInt(4); i++) {
-                int xPos = x + i;
-                for (int j = -rand.nextInt(12) - rand.nextInt(i != 3 ? 1 : 5); j < rand.nextInt(14) + rand.nextInt(1); j++) {
-                    int zPos = z + j;
-                    if (coralCanReplace(world.getBlock(xPos, y - 1, zPos)) &&
-                        BlockHelper.isWater(world, xPos, y + 1, zPos) &&
-                        BlockHelper.isWater(world, xPos, y + 2, zPos)) {
-                        setBlock(cache, world, xPos, y - 1, zPos);
+            generateExtraCorals(world, rand, x, y, z, coralPositions);
+        }
+
+        generateCoralPositions(world, rand, x, z, coralPositions);
+
+        placeCorals(world, rand, coralPositions);
+
+        if (Library.DEBUG_ON) {
+            TimeMeasurement.finish("CoralReef");
+        }
+
+        return true;
+    }
+
+    private void generateExtraCorals(World world, Random rand, int x, int y, int z, List<Integer> coralPositions) {
+        for (int i = -rand.nextInt(16) - rand.nextInt(4); i < rand.nextInt(16) + rand.nextInt(4); i++) {
+            int xPos = x + i;
+            for (int j = -rand.nextInt(12) - rand.nextInt(i != 3 ? 1 : 5); j < rand.nextInt(14) + rand.nextInt(1); j++) {
+                int zPos = z + j;
+                if (world.getChunkProvider().chunkExists(xPos >> 4, zPos >> 4)) {
+                    if (canPlaceCoral(world, xPos, y, zPos)) {
+                        coralPositions.add(xPos);
+                        coralPositions.add(y - 1);
+                        coralPositions.add(zPos);
                     }
                 }
             }
         }
+    }
 
+    private void generateCoralPositions(World world, Random rand, int x, int z, List<Integer> coralPositions) {
         int l = rand.nextInt(5 - 2) + 2;
         for (int i1 = x - l; i1 <= x + l; ++i1) {
             for (int j1 = z - l; j1 <= z + l; ++j1) {
@@ -67,22 +78,23 @@ public class WorldGenReef extends WorldGenerator {
                 int l1 = j1 - z;
 
                 if (k1 * k1 + l1 * l1 <= l * l) {
-                    for (int i2 = y; i2 <= y; ++i2) {
-                        if (coralCanReplace(world.getBlock(i1, i2 - 1, j1)) &&
-                            BlockHelper.isWater(world, i1, i2 + 1, j1) &&
-                            BlockHelper.isWater(world, i1, i2 + 2, j1)) {
-                            setBlock(cache, world, i1, i2 - 1, j1);
-                        }
+                    int yBlock = world.getTopSolidOrLiquidBlock(i1, j1);
+                    if (canPlaceCoral(world, i1, yBlock, j1)) {
+                        coralPositions.add(i1);
+                        coralPositions.add(yBlock - 1);
+                        coralPositions.add(j1);
                     }
                 }
             }
         }
+    }
 
-        for (String s : cache) {
-            String[] coords = s.split(":");
-            int x2 = Integer.parseInt(coords[0]);
-            int z2 = Integer.parseInt(coords[1]);
-            int y2 = world.getTopSolidOrLiquidBlock(x2, z2);
+    private void placeCorals(World world, Random rand, List<Integer> coralPositions) {
+        for (int i = 0; i < coralPositions.size(); i += 3) {
+            int x2 = coralPositions.get(i);
+            int y2 = coralPositions.get(i + 1);
+            int z2 = coralPositions.get(i + 2);
+
             if (BlockHelper.isWater(world, x2, y2 + 2, z2) && BlockHelper.isWater(world, x2, y2 + 1, z2)) {
                 if (rand.nextInt(25) > 0) {
                     world.setBlock(x2, y2, z2, WorldPlus.plantStatic, rand.nextInt(CoralMeta.COUNT - 5) + 2, 2);
@@ -91,11 +103,11 @@ public class WorldGenReef extends WorldGenerator {
                 }
             }
         }
+    }
 
-        if (Library.DEBUG_ON) {
-            TimeMeasurement.finish("CoralReef");
-        }
-
-        return true;
+    private boolean canPlaceCoral(World world, int x, int y, int z) {
+        return coralCanReplace(world.getBlock(x, y - 1, z)) &&
+            BlockHelper.isWater(world, x, y + 1, z) &&
+            BlockHelper.isWater(world, x, y + 2, z);
     }
 }
